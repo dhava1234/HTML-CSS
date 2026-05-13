be4b1f8f06268db730097cf7b8a913dd9dc76d3a
dhava1234 <dhavaagsellaaa@gmail.com>
2026-05-13 14:58:07 +0700

    Add initial html and javascript files

---
+++ JS-Dasar/pertemuan 3/Javascript.js
@@ -0,0 +1,6 @@
+// Javascript.js
+let hello = "Hello World! JayJay";
+document.getElementById("demo").innerHTML = hello;
+
+var fullName = "Muhammad Dhava Agsella";
+console.log(fullName);  // Ini akan muncul di Console browser, bukan terminal
